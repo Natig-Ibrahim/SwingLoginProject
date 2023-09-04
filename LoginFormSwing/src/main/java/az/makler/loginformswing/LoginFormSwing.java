31f70d10293a2cf635331b3a279dac2b20290991
Natig <115658472+Natig-Ibrahim@users.noreply.github.com>
2023-09-04 10:04:34 +0400

    Add files via upload

---
+++ LoginFormSwing/src/main/java/az/makler/loginformswing/LoginFormSwing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package az.makler.loginformswing;
+
+/**
+ *
+ * @author Hp electron
+ */
+public class LoginFormSwing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
